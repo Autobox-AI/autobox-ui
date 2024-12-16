f1dbb357d83dbda20c34e2db4bd77cd671bb1be6
Martín D'Agostino <martin.dagostino@klarna.com>
2024-12-16 11:17:20 +0100

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,11 @@ import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import '../styles/globals.css'
 
-import Sidebar from '@/components/Sidebar'
+import { AppSidebar } from '@/components/app-sidebar'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { cn } from '@/lib/utils'
 import { Organization } from '@/schemas/organization'
+import { cookies } from 'next/headers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +23,8 @@ async function fetchOrganizations(): Promise<Organization[]> {
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const organizations = await fetchOrganizations()
+  const cookieStore = await cookies()
+  const defaultOpen = cookieStore.get('sidebar:state')?.value === 'true'
 
   return (
     <html lang="en" suppressHydrationWarning className="dark">
@@ -35,10 +39,16 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           inter.className
         )}
       >
-        {' '}
         <div className="flex h-screen">
-          <Sidebar organizations={organizations} />
-          <div className="flex-1 p-8 bg-background">{children}</div>
+          <SidebarProvider defaultOpen={defaultOpen}>
+            <AppSidebar />
+            <div className="flex flex-col items-center mt-2">
+              <SidebarTrigger className="p-2 hover:bg-gray-600 text-white" />
+            </div>
+            <main className="flex-1 flex flex-col items-center justify-center p-8 bg-background">
+              {children}
+            </main>
+          </SidebarProvider>
         </div>
       </body>
     </html>
